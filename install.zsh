9eedb50ac267699d035eac0093fd15f8a834a2bd
kanaru-ssk <kanaru.ssk@gmail.com>
2026-06-15 09:55:42 +0900

    chore: update install script to handle path configurations and dependency setup

--- install.zsh
+++ install.zsh
@@ -10,7 +10,7 @@ do
   filename=$(basename "$file")
 
   # 除外リストに含まれているかチェック
-  if [[ ".git .gitignore .DS_Store .config .gemini" =~ "${filename}" ]]; then
+  if [[ ".git .gitignore .DS_Store .config .agents" =~ "${filename}" ]]; then
       continue
   fi
 
@@ -41,8 +41,25 @@ do
   echo "link ~/.config/$filename -> $file"
 done
 
+if [[ ! -d ${HOME}/.agents ]]; then
+  mkdir ${HOME}/.agents
+fi
+for file in "$DOT_DIR"/.agents/*
+do
+  filename=$(basename "$file")
+
+  # すでに存在する場合は削除
+  if [ -e "$HOME/.agents/$filename" ]; then
+    rm -rf "$HOME/.agents/$filename"
+  fi
+
+  # シンボリックリンクを作成
+  ln -snf "$file" "$HOME/.agents/$filename"
+  echo "link ~/.agents/$filename -> $file"
+done
+
 echo "Install brew packages..."
 
-brew bundle --file=$DOT_DIR/Brewfile
+# brew bundle --file=$DOT_DIR/Brewfile
 
 echo "Done!"
