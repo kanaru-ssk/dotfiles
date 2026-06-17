b9150b503e136a83e32dc4716c836757c4f5411b
kanaru-ssk <kanaru.ssk@gmail.com>
2026-06-17 13:46:37 +0900

    feat: add monitorcontrol and visual-studio-code to Brewfile and update dump script

--- dump-brewfile.zsh
+++ dump-brewfile.zsh
@@ -3,4 +3,4 @@
 # dotfilesディレクトリの場所
 DOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-brew bundle dump --file=$DOT_DIR/Brewfile --force --no-go --no-npm
+brew bundle dump --file=$DOT_DIR/Brewfile --force --no-go --no-npm --no-describe
