14695c5f96909070869446f0fa4b6271ab7b0608
kanaru-ssk <kanaru.ssk@gmail.com>
2026-06-24 06:48:55 +0900

    uninstall unused packages

--- dump-brewfile.zsh
+++ dump-brewfile.zsh
@@ -3,4 +3,4 @@
 # dotfilesディレクトリの場所
 DOT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-brew bundle dump --file=$DOT_DIR/Brewfile --force --no-describe --no-vscode --no-go --no-npm
+brew bundle dump --file=$DOT_DIR/Brewfile --force --no-describe --no-go --no-npm
